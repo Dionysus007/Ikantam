a98dd04a79fc188f5916a415c399383146085cab
Dionysus007 <dimosbarbos@mail.ru>
2015-04-13 16:09:11 +0300

    Revert "1"
    This reverts commit 1acd1cb6831f88e4ee8616d6ca7df41c2d17afe9.

--- inc/inc_menu.php
+++
@@ -1,9 +0,0 @@
-
-<ul>
-    <li><a href='index.php'>Домой</a></li>
-    <li><a href='registration.php'>Регистрация</a></li>
-    <li><a href='authorization.php'>Авторизация</a></li>
-    <li><a href='test.php'>Работа с файлами</a></li>
-    <li><a href='exit.php'>Выйти</a></li>
-</ul>
-
